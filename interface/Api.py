6755d9c6a701d02c647fc1033db6a745b7848694
D.J Kong <kdj@yeah.net>
2016-01-19 15:35:36 +0800

    Add Product API

---
+++ interface/Api.py
@@ -0,0 +1,43 @@
+from flask import Flask,redirect,request
+import random
+
+app = Flask("__name__")
+
+clients = ["10.44.147.219:8000","10.51.75.12:8000"]
+
+num = len(clients)
+i = 0
+
+def choose():
+	global i
+	i = i + 1
+	mod = i % num 
+	return mod
+
+@app.route("/wakeup/",methods=["GET"])
+def wake():
+	i = choose()
+	url = "http://%s/wakeup/" % clients[i]
+	return redirect(url)
+
+@app.route("/filter/<sample_no>/",methods=["GET"])
+def filt_vars(sample_no):
+	i = choose()
+	url = "http://%s/filter/%s/" % (clients[i],sample_no)
+	return redirect(url,code=307)
+
+@app.route("/get/<sample_no>/",methods=["GET"])
+def get_vars(sample_no):
+	i = choose()
+	url = "http://%s/get/%s/" % (clients[i],sample_no)
+	return redirect(url)
+
+@app.route("/head/<sample_no>/",methods=["GET"])
+def head_vars(sample_no):
+	i = choose()
+	url = "http://%s/head/%s/" % (clients[i],sample_no)
+	return redirect(url)
+
+
+if __name__ == "__main__":
+	app.run()
