f8cfbaa6974ebfa6afa1a427bbe43ddb6b1b0343
D.J Kong <kdj@yeah.net>
2016-01-30 15:32:18 +0800

    run.sh in nohup way

--- interface/run.sh
+++ interface/run.sh
@@ -1,1 +1,1 @@
-gunicorn -w 4 -k gevent -b  0.0.0.0:8000  Api:app
+nohup gunicorn -w 4 -k gevent -b  0.0.0.0:8000  Api:app 1>>run.log 2>>run.log &
--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-gunicorn -w 4 -k gevent -b  0.0.0.0:8001  VarsService:app
+nohup gunicorn -w 4 -k gevent -b  0.0.0.0:8001  VarsService:app 1>>run.log 2>>run.log &
