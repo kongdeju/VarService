44bf7befefa86b61ae78755212b17ac8725364d5
D.J Kong <kdj@yeah.net>
2016-02-18 15:50:22 +0800

    hpo支持all等

--- modules/hpo2gene/hps2genes.py
+++ modules/hpo2gene/hps2genes.py
@@ -1,22 +1,31 @@
 import msgpack
 
 hpofp = open("modules/hpo2gene/hp2gene.bson",'r')
+genesfp = open("modules/hpo2gene/genenames.bson",'r')
 hp2gene = msgpack.unpackb(hpofp.read())
+Genes = msgpack.unpackb(genesfp.read())
+Genes = set(Genes)
 
 def hps2genes(hps):
 	mod = hps[0]
-	totalgenes = set(hp2gene[hps[1]])
+	try:
+		totalgenes = set(hp2gene[hps[1]])
+	except:
+		totalgenes = Genes
+
 	for hp in hps[2:]:
 		if mod == 'intersect':
 			try:
 				genes = set(hp2gene[hp])
 				totalgenes = totalgenes & genes
 			except:
-				pass
+				genes = Genes
+				totalgenes = totalgenes & genes
 		elif mod == 'join':
 			try:
 				genes = set(hp2gene[hp])
 				totalgenes = totalgenes | genes
 			except:
-				pass
+				genes = Genes
+				totalgenes = totalgenes | genes
 	return totalgenes
