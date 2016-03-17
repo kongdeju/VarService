8fbf052677411881c20854d51d864b530cdad8dd
D.J Kong <kdj@yeah.net>
2016-03-17 10:33:54 +0800

    add sex chrome filter

--- FiltVars.py
+++ FiltVars.py
@@ -17,171 +17,171 @@ sex_pat = re.compile('and Sex == (\w)')
 pheo_pat = re.compile('Phenotype in (\[.+?\])')
 
 def load_bson(filename):
-	fp = open(filename,'rb')
-	vas = msgpack.unpackb(fp.read())
-	return vas
+    fp = open(filename,'rb')
+    vas = msgpack.unpackb(fp.read())
+    return vas
 
 def path2genes(paths):
-	fp = open("lib/path2genes.bson",'rb')
-	p2g_dict = msgpack.unpackb(fp.read())
-	Genes = []
-	for path in paths:
-		try:
-			genes = p2g_dict[path]
-			Genes.extend(genes)
-		except:
-			pass
-	return Genes
+    fp = open("lib/path2genes.bson",'rb')
+    p2g_dict = msgpack.unpackb(fp.read())
+    Genes = []
+    for path in paths:
+        try:
+            genes = p2g_dict[path]
+            Genes.extend(genes)
+        except:
+            pass
+    return Genes
 
 def make_cmd(head_items,filt_str):
-	if filt_str == "All":
-		mengdel_filt = None
-		sex = "U"
-		filt_cmd = '1'
-		return filt_cmd,mengdel_filt,sex
+    if filt_str == "All":
+        mengdel_filt = None
+        sex = "U"
+        filt_cmd = '1'
+        return filt_cmd,mengdel_filt,sex
 
-	mat1 = gene_pat.search(filt_str)
-	mat2 = path_pat.search(filt_str)
-	mat3_1 = mengdel_pat1.search(filt_str)
-	mat3_2 = mengdel_pat2.search(filt_str)
-	mat4 = sex_pat.search(filt_str)
-	mat5 = pheo_pat.search(filt_str)
+    mat1 = gene_pat.search(filt_str)
+    mat2 = path_pat.search(filt_str)
+    mat3_1 = mengdel_pat1.search(filt_str)
+    mat3_2 = mengdel_pat2.search(filt_str)
+    mat4 = sex_pat.search(filt_str)
+    mat5 = pheo_pat.search(filt_str)
 
-	if mat1:
-		gene_list = eval(mat1.group(1))
-		global Genes1
-		Genes1 = set(gene_list)
-		filt_str,n1 = gene_pat.subn("Gene in Genes1",filt_str)
-	if mat2:
-		path_list = eval(mat2.group(1))
-		gene_list = path2genes(path_list)
-		global Genes2
-		Genes2 = set(gene_list)
-		filt_str,n2 = path_pat.subn("Gene in Genes2",filt_str)
-	if mat5:
-		pheno_list = eval(mat5.group(1))
-		gene_set = hps2genes(pheno_list)
-		global Genes3
-		Genes3 = gene_set
-		filt_str = pheo_pat.sub("Gene in Genes3",filt_str)
-	if mat3_1:
-		mengdel_filt = mat3_1.group(1)
-		filt_str = '1'
-	else:
-		if mat3_2:	
-			mengdel_filt = mat3_2.group(1)
-			filt_str = mengdel_pat2.sub('',filt_str)
-		else:
-			mengdel_filt = None
+    if mat1:
+        gene_list = eval(mat1.group(1))
+        global Genes1
+        Genes1 = set(gene_list)
+        filt_str,n1 = gene_pat.subn("Gene in Genes1",filt_str)
+    if mat2:
+        path_list = eval(mat2.group(1))
+        gene_list = path2genes(path_list)
+        global Genes2
+        Genes2 = set(gene_list)
+        filt_str,n2 = path_pat.subn("Gene in Genes2",filt_str)
+    if mat5:
+        pheno_list = eval(mat5.group(1))
+        gene_set = hps2genes(pheno_list)
+        global Genes3
+        Genes3 = gene_set
+        filt_str = pheo_pat.sub("Gene in Genes3",filt_str)
+    if mat3_1:
+        mengdel_filt = mat3_1.group(1)
+        filt_str = '1'
+    else:
+        if mat3_2:    
+            mengdel_filt = mat3_2.group(1)
+            filt_str = mengdel_pat2.sub('',filt_str)
+        else:
+            mengdel_filt = None
 
-	if mat4:
-		sex = mat4.group(1)
-		filt_str = sex_pat.sub('',filt_str)
-	else:
-		sex = "U"
+    if mat4:
+        sex = mat4.group(1)
+        filt_str = sex_pat.sub('',filt_str)
+    else:
+        sex = "U"
 
-	filt_items = filt_str.split()
-	if not filt_items:
-		filt_cmd = '1'
-		mengdel_filt = None
-		sex = "U"
-		return filt_cmd,mengdel_filt,sex
-	i = -1
-	for item in filt_items:
-		i = i + 1
-		if item == "==" or item == "!=" :
-			key_idx1 = head_items.index(filt_items[i-1])
-			filt_items[i-1] = "item%d" % key_idx1
-			if filt_items[i+1] == "null":
-				filt_items[i+1] = "''"
-			else:
-				filt_items[i+1] = "'%s'" % filt_items[i+1]
-		if item == "has":
-			key_idx1 = head_items.index(filt_items[i-1])
-			filt_items[i-1] = "'%s'" % filt_items[i+1]
-			filt_items[i+1] = "item%d" % key_idx1
-			filt_items[i] = "in"
-		if item == "in":
-			key_idx1 = head_items.index(filt_items[i-1])
-			filt_items[i-1] = "item%d" % key_idx1
-		if  ">" in item or "<" in item:
-			key_idx1 = head_items.index(filt_items[i-1])
-			filt_items[i-1] = "Float(item%d)" % key_idx1
-			filt_items[i+1] = "%s" % filt_items[i+1]
-	filt_cmd = " ".join(filt_items)
-	return filt_cmd,mengdel_filt,sex
-			
+    filt_items = filt_str.split()
+    if not filt_items:
+        filt_cmd = '1'
+        mengdel_filt = None
+        sex = "U"
+        return filt_cmd,mengdel_filt,sex
+    i = -1
+    for item in filt_items:
+        i = i + 1
+        if item == "==" or item == "!=" :
+            key_idx1 = head_items.index(filt_items[i-1])
+            filt_items[i-1] = "item%d" % key_idx1
+            if filt_items[i+1] == "null":
+                filt_items[i+1] = "''"
+            else:
+                filt_items[i+1] = "'%s'" % filt_items[i+1]
+        if item == "has":
+            key_idx1 = head_items.index(filt_items[i-1])
+            filt_items[i-1] = "'%s'" % filt_items[i+1]
+            filt_items[i+1] = "item%d" % key_idx1
+            filt_items[i] = "in"
+        if item == "in":
+            key_idx1 = head_items.index(filt_items[i-1])
+            filt_items[i-1] = "item%d" % key_idx1
+        if  ">" in item or "<" in item:
+            key_idx1 = head_items.index(filt_items[i-1])
+            filt_items[i-1] = "Float(item%d)" % key_idx1
+            filt_items[i+1] = "%s" % filt_items[i+1]
+    filt_cmd = " ".join(filt_items)
+    return filt_cmd,mengdel_filt,sex
+            
 def Float(str):
-	try:
-		fnum = float(str)
-	except:
-		fnum =''
-	return fnum
+    try:
+        fnum = float(str)
+    except:
+        fnum =''
+    return fnum
 
 def filt_vars(vas,filt_str):
-	head_items = vas[0]
-	filt_cmd,mengdel,sex = make_cmd(head_items,filt_str)
-	filtvars = []
-	cmd = "for items in vas[1:]:\n "
-	for i in range(len(head_items)):
-		cmd = cmd  + "\titem%s = items[%s]\n" % (i,i)
-	cmd = cmd + "\tif %s :\n" %  filt_cmd
-	cmd = cmd + "\t\tfiltvars.append(items)\n"
-	cmd = cmd + "filtvars.insert(0,head_items)"
-	exec(cmd)
-	print cmd
-	return filtvars,mengdel,sex
+    head_items = vas[0]
+    filt_cmd,mengdel,sex = make_cmd(head_items,filt_str)
+    filtvars = []
+    cmd = "for items in vas[1:]:\n "
+    for i in range(len(head_items)):
+        cmd = cmd  + "\titem%s = items[%s]\n" % (i,i)
+    cmd = cmd + "\tif %s :\n" %  filt_cmd
+    cmd = cmd + "\t\tfiltvars.append(items)\n"
+    cmd = cmd + "filtvars.insert(0,head_items)"
+    exec(cmd)
+    print cmd
+    return filtvars,mengdel,sex
 
 def mengdel_vars(vars,filt_str,sex):
-	if filt_str :
-		vars = secondfilt.secondfilt(vars,filt_str,sex)
-		return vars
-	else:
-		return vars
+    if filt_str :
+        vars = secondfilt.secondfilt(vars,filt_str,sex)
+        return vars
+    else:
+        return vars
 
 def FiltVars(sample_no,filt_str):
-	filename = sample_no + '.bson'
-	file_path = os.path.join(Path,filename)
-	try: 
-		vas = load_bson(file_path)
-		try:
-			filts,mengdel,sex = filt_vars(vas,filt_str)
-			filts = mengdel_vars(filts,mengdel,sex)
-			status = "Success"
-			return status,filts
-		except:
-			
-			status = "Internal Error,check your submit %s:%s" % (sys.exc_info()[0],sys.exc_info()[1])
-			filts = "NA"
-			return status,filts
-	except:
-		status =  "%s is not annotated" % sample_no
-		filts = "NA"
-		return status,filts
+    filename = sample_no + '.bson'
+    file_path = os.path.join(Path,filename)
+    try: 
+        vas = load_bson(file_path)
+        try:
+            filts,mengdel,sex = filt_vars(vas,filt_str)
+            filts = mengdel_vars(filts,mengdel,sex)
+            status = "Success"
+            return status,filts
+        except:
+            
+            status = "Internal Error,check your submit %s:%s" % (sys.exc_info()[0],sys.exc_info()[1])
+            filts = "NA"
+            return status,filts
+    except:
+        status =  "%s is not annotated" % sample_no
+        filts = "NA"
+        return status,filts
 
 def HeadVars(sample_no):
-	filename = sample_no + '-top.bson'
-	file_path = os.path.join(Path,filename)
-	try:
-		vas = load_bson(file_path)
-		status = "Success"
-		return status,vas
-	except:
-		status =  "%s is not annotated" % sample_no
-		vas = "NA"
-		return status,vas
+    filename = sample_no + '-top.bson'
+    file_path = os.path.join(Path,filename)
+    try:
+        vas = load_bson(file_path)
+        status = "Success"
+        return status,vas
+    except:
+        status =  "%s is not annotated" % sample_no
+        vas = "NA"
+        return status,vas
 
 def LoadVars(sample_no):
-	filename = sample_no + '.bson'
-	file_path = os.path.join(Path,filename)
-	try:
-		vas = load_bson(file_path)
-		status = "Success"
-		return status,vas
-	except:
-		status =  "%s is not annotated" % sample_no
-		vas = "NA"
-		return status,vas
+    filename = sample_no + '.bson'
+    file_path = os.path.join(Path,filename)
+    try:
+        vas = load_bson(file_path)
+        status = "Success"
+        return status,vas
+    except:
+        status =  "%s is not annotated" % sample_no
+        vas = "NA"
+        return status,vas
 
 #if __name__ == "__main__":
-#	LoadVars('WGC050803D')	
+#    LoadVars('WGC050803D')    
--- modules/modeofinhere/secondfilt.py
+++ modules/modeofinhere/secondfilt.py
@@ -1,102 +1,170 @@
 from mendian import GeneMode
 from collections import defaultdict
-
 def dictify(variants):
-	head = variants[0]
-	gene_idx = head.index("Gene")
-	gt_idx = head.index("GenoType")
-	chr_idx = head.index("Chr")
-	gene_gts = defaultdict(list)
-	gene_chrs = defaultdict(list)
-	gene_mods = defaultdict(list)
-	for var in variants[1:]:
-		gene = var[gene_idx]
-		gt = var[gt_idx]
-		chr = var[chr_idx]
-		mod = GeneMode(gene)
-		gene_gts[gene].append(gt)
-		gene_chrs[gene].append(chr)
-		gene_mods[gene].append(mod)
-	return gene_gts,gene_chrs,gene_mods	
+    head = variants[0]
+    gene_idx = head.index("Gene")
+    gt_idx = head.index("GenoType")
+    chr_idx = head.index("Chr")
+    gene_gts = defaultdict(list)
+    gene_chrs = defaultdict(list)
+    gene_mods = defaultdict(list)
+    for var in variants[1:]:
+        gene = var[gene_idx]
+        gt = var[gt_idx]
+        chr = var[chr_idx]
+        mod = GeneMode(gene)
+        gene_gts[gene].append(gt)
+        gene_chrs[gene].append(chr)
+        gene_mods[gene].append(mod)
+    return gene_gts,gene_chrs,gene_mods    
 
+##Norm_AD_
+def norm_ad(mod,mchr,nhet,nhom):
+    if "AD" in mod:
+        modstr = "AD"
+    elif "UN" in mod or "AU" in mod  or  "AO" in mod :
+        modstr = "PAD"
+    else:
+        return None
+    if mchr == "X" or mchr == "Y":
+        return None
+    if nhet == 1 and nhom == 0:
+        degree = "Highly"
+    if nhet + nhom >= 2:
+        degree = "Maybe"
+    if nhet == 0 and nhom == 1:
+        degree = "Cannot"
+    if degree:
+        lable = mchr + '_' + modstr + "_" + degree
+        return lable
+def norm_ar(mod,mchr,nhet,nhom):
+    if "AR" in mod:
+        modstr = "AR"
+    elif "UN" in mod or "AU" in mod  or "AO" in mod:
+        modstr = "PAR"
+    else:
+        return None
+    if mchr == "X" or mchr == "Y":
+        return None
+    if ( nhet == 2 and nhom == 0 ) or ( nhom == 1 and nhet == 0 ):
+        degree = "Highly"
+    if nhet + nhom > 2:
+        degree = "Maybe"
+    if nhet == 1 and nhom == 0:
+        degree = "Cannot"
+    if degree:
+        lable = mchr + '_' + modstr + "_" + degree
+        return lable
+def x_ad(mod,mchr,nhet,nhom,sex):
+    if "XR" in mod:
+        modstr = "AR"
+    elif "UN" in mod or "XU" in mod  or "XO" in mod or "XL" in mod :
+        modstr = "PAR"
+    else:
+        return None
+    if mchr == "Norm":
+        return None
+    if set == "U":
+        return None
+    degree = None
+    if sex == "W":
+        if nhet == 1 and nhom == 0:
+            degree = "Highly"
+        if nhet + nhom >= 2 :
+            degree = "Maybe"
+        if nhet == 0 and nhom == 1:
+            degree = "Cannot"
+        if degree:
+            lable = mchr + '_' + modstr + '_' + degree
+            return lable
+    if sex == "M":
+        if nhet == 0 and nhom == 1:
+            degree = "Highly"
+        if nhet ==0 and nhom > 1:
+            degree = "Maybe"
+        if nhet >=1:
+            degree = "Cannot"
+        if degree:
+            lable = mchr + '_' + modstr + '_' + degree
+            return lable
+
+def x_ar(mod,mchr,nhet,nhom,sex):
+    if "XR" in mod:
+        modstr = "AR"
+    elif "UN" in mod or "XU" in mod or "XO" in mod or "XL" in mod:
+        modstr = "PAR"
+    else:
+        return None
+    if mchr == "Norm":
+        return None
+    if set == "U":
+        return None
+    degree = None
+    if sex == "W":
+        if (nhet == 2 and nhom == 0) or (nhom == 1 and nhet == 0):
+            degree = "Highly"
+        if nhet + nhom > 2 :
+            degree = "Maybe"
+        if nhet == 1 and nhom == 0:
+            degree = "Cannot"
+        if degree:
+            lable = mchr + '_' + modstr + '_' + degree
+            return lable
+    if sex == "M":
+        if  nhom == 1:
+            degree = "Highly"
+        if  nhom > 1:
+            degree = "Maybe"
+        if nhom == 0:
+            degree = "Cannot"
+        if degree:
+            lable = mchr + '_' + modstr + '_' + degree
+            return lable
+    
 def labelgene(gene_gts,gene_chrs,gene_mods,sex):
-	genelable = {}
-	for gene in gene_gts.keys():
-		nhet = 0
-		nhom = 0
-		mchr = ''
-		lables =[]
-		for gt in gene_gts[gene]:
-			if gt == "0|0" or gt == "0/0" or gt == "1/1" or gt == "1|1":
-				nhom = nhom + 1
-			else:
-				nhet = nhet + 1
-		nvar = len(gene_gts[gene])
-		mod = gene_mods[gene][0]
-		chr = gene_chrs[gene][0]
-		degree = ''
+    genelable = {}
+    for gene in gene_gts.keys():
+        nhet = 0
+        nhom = 0
+        mchr = ''
+        for gt in gene_gts[gene]:
+            if gt == "0|0" or gt == "0/0" or gt == "1/1" or gt == "1|1":
+                nhom = nhom + 1
+            else:
+                nhet = nhet + 1
+        nvar = len(gene_gts[gene])
+        mod = gene_mods[gene][0]
+        chr = gene_chrs[gene][0]
+        degree = ''
 
-		if chr != 'X' and chr != 'Y':
-			mchr = "Norm"
-		else:
-			mchr = chr
-		
-		if ( "D" in mod or "U" in mod or "O" in mod or "L" in mod) and ( mchr == "Norm" or ( mchr == "X" and sex == "W" )):
-			if "D" in mod:
-				modstr = "AD"
-			else:
-				modstr = "PAD"
-			if nhet == 1 and nhom == 0:
-				degree = "Highly"
-			if nhet  + nhom >= 2:
-				degree = "Maybe"
-			if nhet == 0 and  nhom == 1:
-				degree = "Cannot"
-			lable = mchr + '_' + modstr + "_" + degree
-			lables.append(lable)
-
-		if ("R" in mod or "U" in mod or "O" in mod or "L" in mod ) and ( mchr == "Norm" or ( mchr == "X" and sex == "W")):
-			if "R" in mod:
-				modstr = "AR"
-			else:
-				modstr = "PAR"
-			if ( nhet == 2 and nhom == 0 ) or ( nhet == 0 and nhom == 1 ) :
-				degree = "Highly"
-			if nhet + nhom >2:		
-				degree = "Maybe"
-			if nhet == 1 and nhom == 0:
-				degree = "Cannot"
-			lable = mchr + "_" + modstr + "_" + degree
-			lables.append(lable)
-#		if  mchr == "X" and sex == "M":
-#			if nhet + nhom == 1:
-#				degree = "Highly"
-#			if nhet + nhom >=2:
-#				degree = "Maybe"
-#			for item in mod:
-#				modstr = item
-#				lable = mchr + "_" + modstr + "_" + degree	
-#				lables.append(lable)
-		genelable[gene] = lables
-	return genelable
-
-
+        if chr != 'X' and chr != 'Y':
+            mchr = "Norm"
+        else:
+            mchr = chr
+        print mod,mchr,nhet,nhom,sex
+        normad_lb = norm_ad(mod,mchr,nhet,nhom)
+        normar_lb = norm_ad(mod,mchr,nhet,nhom)
+        xad_lb = x_ad(mod,mchr,nhet,nhom,sex)
+        xar_lb = x_ar(mod,mchr,nhet,nhom,sex)
+        lables = [normad_lb,normar_lb,xad_lb,xar_lb]
+        #print lables
+        genelable[gene] = lables
+    return genelable
 
 def secondfilt(variants,filtstr,sex="U"):
 
-	gene_gts,gene_chrs,gene_mods = dictify(variants)	
-	genelable = labelgene(gene_gts,gene_chrs,gene_mods,sex)
-
-	filtset = set(eval(filtstr))
-	head = variants[0]
-	gene_idx = head.index("Gene")
-	filtvars = []
-	filtvars.append(head)
-	for var in variants[1:]:
-		gene = var[gene_idx]
-		lable = set(genelable[gene])
-		if lable & filtset:
-			filtvars.append(var)
-	return filtvars
+    gene_gts,gene_chrs,gene_mods = dictify(variants)    
+    genelable = labelgene(gene_gts,gene_chrs,gene_mods,sex)
+    filtset = set(eval(filtstr))
+    head = variants[0]
+    gene_idx = head.index("Gene")
+    filtvars = []
+    filtvars.append(head)
+    for var in variants[1:]:
+        gene = var[gene_idx]
+        lable = set(genelable[gene])
+        if lable & filtset:
+            filtvars.append(var)
+    return filtvars
 
 
